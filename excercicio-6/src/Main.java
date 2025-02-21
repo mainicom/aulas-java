68e2770296fde72e800227dbec0e638a1e0b3e62
mainicom <hvomeroh@gmail.com>
2025-02-21 12:07:04 -0300

    Add files via upload

---
+++ excercicio-6/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    /// que codigo bobinho kkk
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite um numero");
+        num = scanner.nextInt();
+        System.out.println(num+1);
+        System.out.println(num-1);
+    }
+}
